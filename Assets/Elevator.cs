ac1ce8d7f49cedd26cb273515366a8e0e9535c4c
parkjun <parkjun436@gmail.com>
2023-03-07 00:33:07 +0900

    Revert "add map file"
    This reverts commit 7baa8881ab32156172cb5d4a3e8bd15b49a1948e.

--- Assets/Elevator.cs
+++ Assets/Elevator.cs
@@ -1,13 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
+//using UnityEditor.Animations;
 using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
-    public static Elevator elevator;
-
     [Header("Button")]
     public GameObject button1f;
     public GameObject button2f;
@@ -25,40 +23,97 @@ public class Elevator : MonoBehaviour
 
     public Animator[] evanim;
 
-    public bool isup = false;
-    
+    private String btntag;
+    private uint isup=0;
     // Start is called before the first frame update
-    public void Start()
+    void Start()
     {
-        if(elevator == null)
-        {
-            elevator = this;
-        }
         evanim = GetComponentsInChildren<Animator>();
         
     }
 
-    public void Eledooropen1f()
+    // Update is called once per frame
+    void FixedUpdate()
     {
-        evanim[0].SetTrigger("isClick");
-        evanim[1].SetTrigger("isleft");
-    }
-    public void Eledooropen2f()
-    {
-        evanim[2].SetTrigger("isClick");
-        evanim[3].SetTrigger("isleft");
+        buttonray();
     }
 
-    public void Eleup()
+    void buttonray()
     {
-        evanim[4].SetTrigger("Up");     
-        isup = true;
-    }
+        Ray btnray1 = new Ray(button1f.transform.position - new Vector3(0,1.0f,0), transform.right);
+        RaycastHit hitinfo;
+        Debug.DrawRay(button1f.transform.position - new Vector3(0, 1.0f, 0), transform.right, new Color(1,0,0));
+        if (Physics.Raycast(btnray1, out hitinfo, 1f))
+        {
+            btntag = hitinfo.collider.tag;
+            if (btntag == "Player" && Input.GetButtonDown("interact") && isup == 0)
+            {
+                Debug.Log(btntag);
+                evanim[0].SetTrigger("isClick");
+                evanim[1].SetTrigger("isClick");
+            }
+            else if(btntag == "Player" && Input.GetButtonDown("interact") && isup == 1)
+            {
+                evanim[2].SetTrigger("isClick");
+                evanim[3].SetTrigger("isClick");
+            }
+        }
 
-    public void Eledown()
-    {
-         evanim[4].SetTrigger("Down");
-         isup = false;
-    }
+        Ray btnray2 = new Ray(button2f.transform.position - new Vector3(0, 1.0f, 0), transform.right);
+        RaycastHit hitinfo2;
+        Debug.DrawRay(button2f.transform.position - new Vector3(0, 1.0f, 0), transform.right, new Color(1, 0, 0));
+        if (Physics.Raycast(btnray2, out hitinfo2, 1f))
+        {
+            btntag = hitinfo2.collider.tag;
+            if (btntag == "Player" && Input.GetButtonDown("interact") && isup == 0)
+            {
+                Debug.Log(btntag);
+                evanim[0].SetTrigger("isClick");
+                evanim[1].SetTrigger("isClick");
+            }
+            else if (btntag == "Player" && Input.GetButtonDown("interact") && isup == 1)
+            {
+                evanim[2].SetTrigger("isClick");
+                evanim[3].SetTrigger("isClick");
+            }
+        }
 
+        Ray btnray3 = new Ray(buttonin.transform.position, transform.right * -1);
+        RaycastHit hitinfo3;
+        Debug.DrawRay(buttonin.transform.position, transform.right * -1, new Color(1, 0, 0));
+        if (Physics.Raycast(btnray3, out hitinfo3, 1f))
+        {
+            btntag = hitinfo3.collider.tag;
+            if (btntag == "Player" && Input.GetButtonDown("interact") && isup == 0)
+            {
+                Debug.Log(btntag);
+                evanim[0].SetTrigger("isClick");
+                evanim[1].SetTrigger("isClick");
+            }
+            else if (btntag == "Player" && Input.GetButtonDown("interact") && isup == 1)
+            {
+                evanim[2].SetTrigger("isClick");
+                evanim[3].SetTrigger("isClick");
+            }
+        }
+
+        Ray btnray4 = new Ray(buttonupdown.transform.position + new Vector3(0.6f, 1.0f, -0.2f), transform.forward);
+        RaycastHit hitinfo4;
+        Debug.DrawRay(buttonupdown.transform.position + new Vector3(0.6f, 1.0f, -0.2f), transform.forward, new Color(1, 0, 0));
+        if (Physics.Raycast(btnray4, out hitinfo4, 1f))
+        {
+            btntag = hitinfo4.collider.tag;
+            if (btntag == "Player" && Input.GetButtonDown("interact") && isup == 0)
+            {              
+                evanim[4].SetTrigger("Up");
+                isup = 1;
+            }
+            else if(btntag == "Player" && Input.GetButtonDown("interact") && isup == 1)
+            {                
+                evanim[4].SetTrigger("Down");
+                isup = 0;
+            }
+        }
+    }
 }
+
