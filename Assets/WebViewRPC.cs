c4091cfd97d89c71e27b30b00255f74e8e184741
parkjun <parkjun436@gmail.com>
2023-03-06 22:59:14 +0900

    interface UI

--- Assets/WebViewRPC.cs
+++ Assets/WebViewRPC.cs
@@ -33,8 +33,8 @@ public class WebViewRPC : MonoBehaviour
             Debug.Log("[SimpleWebViewDemo] URL changed: " + eventArgs.Url);
             OnURLEvent(eventArgs.Url);
         };
-
     }
+
 
     [PunRPC]
     public void SetURL()
