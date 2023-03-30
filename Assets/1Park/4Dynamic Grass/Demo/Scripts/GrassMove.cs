1c902428c47c826f71bbb4882f6c7053765c19b8
parkjun <parkjun436@gmail.com>
2023-03-30 21:05:00 +0900

    map input

---
+++ Assets/1Park/4Dynamic Grass/Demo/Scripts/GrassMove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassMove : MonoBehaviour {
+
+	void Update ()
+	{
+		transform.position += transform.forward * Time.deltaTime * 4;
+		transform.Rotate(Vector3.up * Time.deltaTime * 64);
+	}
+}
