07beca46e62b42fcb27d5b5d40343aa424b09806
Agujun <kang6828@naver.com>
2023-04-12 18:33:07 +0900

    man1 cloth,hair, customizing script

--- Assets/Scripts/Customizing.cs
+++ Assets/Scripts/Customizing.cs
@@ -2,47 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.ProBuilder.Shapes;
+using Photon.Pun;
 
 public class Customizing : MonoBehaviour
 {
-    [Header("Hair Materials")]
-    [Tooltip("Customizing hair color")]
-    public Material[] hairMt;
+    public GameObject character_body;
+    public Color[] color;
+    [Header("Materials")]
+    [Tooltip("Customizing yout character")]
+    public Material[] body_mat;
+    public Material hair_mat;
+    public Material cloth_mat;
+
+    public PhotonView PV;
 
 
-    // color
-   // private Color[] color = new Color[9];
-   // private int colors = 0;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-     //   if()
-     //  hairMt = this.
+        character_body = this.gameObject;
+        body_mat = character_body.GetComponent<SkinnedMeshRenderer>().materials;
+        hair_mat = body_mat[0];
+        cloth_mat = body_mat[8];
+        color[0] = new Color(150,0,0);  // red
+        color[1] = new Color(0,20,150); // blue
+        color[2] = new Color(200,200,80);  // gold
+        color[3] = new Color(80,30,90);  // violet
+        color[4] = new Color(100,100,100);  // gray
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-      //  customizing();
+        PV.RPC("customizing", RpcTarget.All);
     }
 
-   /* public void customizing()
+    [PunRPC]
+    public void customizing()
     {
-        if (colors <= 8)
+       /* if ()
         {
-            hairMt[0].color = color[++colors];
-            if (hairs2)
+           
             {
-                hairs2.color = color[++colors];
+               
 
             }
         }
         else
         {
-            colors = 0;
+            
 
-        }
-    }*/
+        }*/
+    }
 }
