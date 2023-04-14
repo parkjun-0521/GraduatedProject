7e0bf8eca2ac48aa0f31212e02b95b5d52a389f6
Agujun <kang6828@naver.com>
2023-04-14 19:14:13 +0900

    man1 hair,cloth, Vrm

--- Assets/Scripts/Customizing.cs
+++ Assets/Scripts/Customizing.cs
@@ -7,12 +7,13 @@ using Photon.Pun;
 public class Customizing : MonoBehaviour
 {
     public GameObject character_body;
-    public Color[] color;
+    private Color[] color = new Color[5];
     [Header("Materials")]
-    [Tooltip("Customizing yout character")]
-    public Material[] body_mat;
-    public Material hair_mat;
+    [Tooltip("Customizing your character")]
+    private Material[] body_mat;
+    private Material hair_mat;
     public Material cloth_mat;
+    public Material[] cloth = new Material[3];
 
     public PhotonView PV;
 
@@ -23,22 +24,77 @@ public class Customizing : MonoBehaviour
         character_body = this.gameObject;
         body_mat = character_body.GetComponent<SkinnedMeshRenderer>().materials;
         hair_mat = body_mat[0];
+       // cloth_mat = Instantiate(body_mat[8]);
         cloth_mat = body_mat[8];
-        color[0] = new Color(150,0,0);  // red
-        color[1] = new Color(0,20,150); // blue
-        color[2] = new Color(200,200,80);  // gold
-        color[3] = new Color(80,30,90);  // violet
-        color[4] = new Color(100,100,100);  // gray
+
+        
+        color[0] = new Color(150/255f,0f,0f);  // red
+        color[1] = new Color(0f,20/255f,150/255f); // blue
+        color[2] = new Color(200/255f,200/255f,80/255f);  // gold
+        color[3] = new Color(80/255f,30/255f,90/255f);  // violet
+        color[4] = new Color(130/255f,130/255f,130/255f);  // gray
+
+        
     }
 
     public void Update()
     {
-        PV.RPC("customizing", RpcTarget.All);
+        if (Input.GetButtonDown("interact"))
+        {                
+            if (this.hair_mat.color == color[0])
+            {
+                this.hair_mat.color = color[1];
+            }
+            else if (this.hair_mat.color == color[1])
+            {
+                this.hair_mat.color = color[2];
+            }
+            else if (this.hair_mat.color == color[2])
+            {
+                this.hair_mat.color = color[3];
+            }
+            else if (this.hair_mat.color == color[3])
+            {
+                this.hair_mat.color = color[4];
+            }
+            else if(this.hair_mat.color == color[4])
+            {
+                this.hair_mat.color = new Color(0f, 0f, 0f, 255f);
+            }
+            else
+                this.hair_mat.color = color[0];
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {           
+            if (this.body_mat[8] == cloth[0])
+            {
+                this.body_mat[8] = cloth[1];               
+            }
+            else if (this.body_mat[8] == cloth[1])
+            {
+                this.body_mat[8] = cloth[2];
+            }
+            else
+            {
+                this.body_mat[8] = cloth[0];
+            }
+        }
+
+            //  PV.RPC("customizing", RpcTarget.All);
+            Debug.Log(this.hair_mat.color);   
+            Debug.Log(this.cloth_mat);
+            Debug.Log(this.body_mat[8]);
     }
 
     [PunRPC]
     public void customizing()
     {
+        for(int i=0; i<5; i++)
+        {
+            if (Input.GetButtonDown("interact"))
+                this.hair_mat.color = color[i];
+        }
        /* if ()
         {
            
