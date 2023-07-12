bfd595d4df20d409884f1173bf71dbe238d7e374
parkjun <parkjun11114@gmail.com>
2023-07-12 17:30:49 +0900

    WebViewArrangement CodeComment Complete

--- Assets/my/Scripts/WebViewArrangement.cs
+++ Assets/my/Scripts/WebViewArrangement.cs
@@ -7,17 +7,7 @@ using Photon.Realtime;
 
 public class WebViewArrangement : MonoBehaviourPunCallbacks {
     
-    public GameObject[] webViewScreen;
-    public GameObject[] webViewObject;
-
-
-    void Start()
-    { 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // 웹뷰 동기화를 위한 변수 선언 
+    public GameObject[] webViewScreen;  // 웹뷰를 찍고 있는 카메라 저장 
+    public GameObject[] webViewObject;  // 웹뷰 오브젝트 전체를 저장 
 }
